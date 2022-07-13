b4c92ea74ed810d8d321fd1cef159059079538e2
zuna2005 <nasiba.zulunova@gmail.com>
2022-07-13 20:49:06 +0600

    first commit

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
 BOT_TOKEN = '5589355906:AAGKqO_SFp55xXzGDZGNTYBcjJqPS2UQMl0'
-APP_URL = ' https://tgbottester3.herokuapp.com/' + BOT_TOKEN
+APP_URL = 'https://tgbottester3.herokuapp.com/' + BOT_TOKEN
